07236eb92ee69f2201e50f38024d3ee3ae25ff7f
EclecticWizard <hodgkinsj95@proton.me>
2025-02-23 09:19:08 +0000

    Increase font size and set JetBrains Mono font

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files '("~/Documents/Vault/org/agenda.org"))
  '(package-selected-packages
